06e772782c4d7258f3a63ea918ae149ad4238cb9
diehardkamikaze <diehardkamikaze@gmail.com>
2021-03-06 18:04:05 +0300

    feat: added extended repeater exercise solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+	var result;
+	var fullstr;
+	
+	if (options.separator === undefined)
+		options.separator = '+';
+	if (options.additionSeparator === undefined)
+		options.additionSeparator = '|';
+	if (options.addition === undefined)
+		options.addition = '';
+	result = "";
+	fullstr = "";
+	fullstr = fullstr + options.addition;
+	while (--options.additionRepeatTimes)
+			fullstr = fullstr + options.additionSeparator + options.addition;
+	fullstr = str + fullstr;
+	result = fullstr;
+	while (--options.repeatTimes)
+		result += options.separator + fullstr;
+	return (result);
 };
-  
+
+ 
