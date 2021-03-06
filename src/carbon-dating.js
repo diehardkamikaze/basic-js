c652e7cf7aaa4f715f6d32c6738008dd980880cd
diehardkamikaze <diehardkamikaze@gmail.com>
2021-03-06 18:02:37 +0300

    feat: added carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const HALF_LIFE_PERIOD= 5730; // период полураспада = 5730 лет
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  //sampleAcitivity - активность изотопа в образце 
+  //modern activity - активность изотопа современная, т.е ещё до всех этих полураспадов?
+  //half-life period - период полураспада
+  //каждый распад активность падает в 2 раза
+  if (typeof sampleActivity != "string")
+    return false;
+  var sA = parseFloat(sampleActivity);
+  if (!sA || 0 > sA || sA > 15)
+    return (false);
+  var t = 0.693 / HALF_LIFE_PERIOD;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / t);
 };
